ceec9e4de76a127355cbd445f2bec936189e2d54
CAIO <caio.b.santos@hotmail.com>
2018-10-29 22:07:45 -0300

    First commit

---
+++ projeto/sistema/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100, blank=False)
+    ra = models.PositiveIntegerField(default=123456, blank=False)
+    trancou = models.BooleanField(null=True, blank=True, default=False)
+
+    def __str__(self):
+        return self.nome
